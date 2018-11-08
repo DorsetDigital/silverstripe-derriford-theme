f8dae38454510fed0047463c6d144bb60420c74a
Tim Burt <tim@dorset-digital.net>
2018-11-08 17:36:29 +0000

    Initial Commit

---
+++ src/ConfigExtension.php
@@ -0,0 +1,9 @@
+<?php
+namespace DorsetDigital\Themes\Derriford;
+
+use SilverStripe\ORM\DataExtension;
+
+class ConfigExtension extends DataExtension
+{
+
+}
---
+++ src/PageControllerExtension.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tim
+ * Date: 08/11/18
+ * Time: 15:05
+ */
+
+namespace DorsetDigital\Themes\Derriford;
+
+
+class PageControllerExtension
+{
+
+}
