2c00d109d05bc3a5c9527ba4ac64950daca07911
Tim Burt <tim@dorset-digital.net>
2018-11-29 13:58:13 +0000

    Add slider element

--- client/src/javascript/site.js
+++ client/src/javascript/site.js
@@ -1,8 +1,13 @@
 /* Global Site Scripts */
 
 jQuery(document).ready(function($) {
+
     $('#mobiletoggler').on('click', function() {
        $(this).toggleClass('is-active');
+       $('#navbarNav').toggleClass('visible');
     });
+
+    $('main').css('padding-top', $('header').height());
+
 });
 
